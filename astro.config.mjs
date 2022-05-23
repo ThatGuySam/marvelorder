84dec40eb05a3763c7ccea21d13373f41f2700b2
Sam Carlton <sam@sam.lc>
2022-05-22 22:02:45 -0500

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+import react from '@astrojs/react';
+
+import vue from "@astrojs/vue";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [// Enable Preact to support Preact JSX components.
+  preact(), // Enable React for the Algolia search component.
+  react(), vue(), tailwind(), sitemap()]
+});
