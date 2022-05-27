c0fd6d57f881c95bf79bfa0f2d92547c80d2699d
Sam Carlton <sam@sam.lc>
2022-05-27 18:22:41 -0500

    Update getLayoutModes to pass multiple values

--- src/helpers/layout.ts
+++ src/helpers/layout.ts
@@ -1,13 +1,23 @@
-export function getLayoutMode ( itemIndex ) {
+export function getLayoutModes ( itemIndex ) {
     // Get the layout mode
     const modeNumber = itemIndex % 4
 
-    const modes = {
-        1: 'upper-top',
-        2: 'lower-bottom',
-        3: 'upper-mid',
-        0: 'lower-mid',
+    const outerModes = {
+        1: 'start',
+        2: 'end',
+        3: 'start',
+        0: 'end',
     }
 
-    return modes[ modeNumber ]
+    const innerModes = {
+        1: 'start',
+        2: 'end',
+        3: 'end',
+        0: 'start',
+    }
+
+    return { 
+        outer: outerModes[modeNumber], 
+        inner: innerModes[modeNumber]
+    }
 }
--- test/layout.test.ts
+++ test/layout.test.ts
@@ -1,20 +1,20 @@
 import { assert, expect, test } from 'vitest'
 
 // @ts-ignore
-import { getLayoutMode } from '../src/helpers/layout.ts'
+import { getLayoutModes } from '../src/helpers/layout.ts'
 
 
 test('Math.sqrt()', () => {
 
 
-    expect( getLayoutMode(1) ).toBe( 'upper-top' )
-    expect( getLayoutMode(2) ).toBe( 'lower-bottom' )
-    expect( getLayoutMode(3) ).toBe( 'upper-mid' )
-    expect( getLayoutMode(4) ).toBe( 'lower-mid' )
-    expect( getLayoutMode(5) ).toBe( 'upper-top' )
-    expect( getLayoutMode(6) ).toBe( 'lower-bottom' )
-    expect( getLayoutMode(7) ).toBe( 'upper-mid' )
-    expect( getLayoutMode(8) ).toBe( 'lower-mid' )
-    expect( getLayoutMode(9) ).toBe( 'upper-top' )
-    expect( getLayoutMode(10) ).toBe( 'lower-bottom' )
+    expect( getLayoutModes(1) ).toContain( { outer: 'start', inner: 'start' } )
+    expect( getLayoutModes(2) ).toContain( { outer: 'end', inner: 'end' } )
+    expect( getLayoutModes(3) ).toContain( { outer: 'start', inner: 'end' } )
+    expect( getLayoutModes(4) ).toContain( { outer: 'end', inner: 'start' } )
+    expect( getLayoutModes(5) ).toContain( { outer: 'start', inner: 'start' } )
+    expect( getLayoutModes(6) ).toContain( { outer: 'end', inner: 'end' } )
+    expect( getLayoutModes(7) ).toContain( { outer: 'start', inner: 'end' } )
+    expect( getLayoutModes(8) ).toContain( { outer: 'end', inner: 'start' } )
+    expect( getLayoutModes(9) ).toContain( { outer: 'start', inner: 'start' } )
+    expect( getLayoutModes(10) ).toContain( { outer: 'end', inner: 'end' } )
 })
