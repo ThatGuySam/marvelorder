e0fdea29691968bc10b6427533f9bb892e647c38
Sam Carlton <sam@sam.lc>
2022-05-27 19:57:48 -0500

    Add MappedListing class

--- src/helpers/node/listing.ts
+++ src/helpers/node/listing.ts
@@ -1,7 +1,10 @@
 import { promises as fs } from 'fs'
+import { DateTime } from 'luxon'
 // import fs from 'fs-extra'
 import matter from 'gray-matter'
 
+// @ts-ignore
+import { Listing } from './types.ts'
 import { 
     getPartsFromMarkdown, 
     parseTMDbMarkdown
@@ -29,4 +32,31 @@ export async function getListingFromFile ( filePath: string ) {
     const markdown = await fs.readFile( filePath, 'utf8' )
 
     return getDataFromListingContents( markdown )
+}
+
+
+export class MappedListing {
+    constructor ( listing : Listing ) {
+        this.sourceListing = listing
+
+        // Map properties from the listing
+        for ( const [ key, value ] of Object.entries( listing ) ) {
+            this[key] = value
+        }
+    }
+
+    sourceListing
+
+    get dateString () {
+        return this.sourceListing.release_date || this.sourceListing.first_air_date
+    }
+
+    get date () {
+        return DateTime.fromISO( this.dateString )
+    }
+
+    get year () {
+        return this.date.year
+    }
+    
 }
