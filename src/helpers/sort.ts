f6e0ce44553c38b5e9f8da4a7d11f8179be4a61e
Sam Carlton <sam@sam.lc>
2022-05-29 14:43:52 -0500

    Show undated listings with logos first

--- src/helpers/sort.ts
+++ src/helpers/sort.ts
@@ -2,11 +2,22 @@
 // @ts-ignore
 import { Listing } from './types.ts'
 
+
+const bigOleNumber = 9999999999999999
+
+function hasLogo ( listing:Listing ) {
+    return listing?.logo_on_black
+}
+
 function getTitleDate ( listing:Listing ) {
     if ( listing?.release_date ) {
         return new Date( listing.release_date )
     } else if ( listing?.first_air_date ) {
         return new Date( listing.first_air_date )
+    }
+
+    if ( hasLogo( listing ) ) {
+        return bigOleNumber - 1
     }
 
     // If no release date or first air date
