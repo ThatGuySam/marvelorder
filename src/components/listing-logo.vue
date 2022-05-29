f55077ba084c952d34aa943064acd080f0eaf68c
Sam Carlton <sam@sam.lc>
2022-05-29 18:27:01 -0500

    Round square logos

--- src/components/listing-logo.vue
+++ src/components/listing-logo.vue
@@ -3,7 +3,7 @@
         :data-src="smallestImage"
         :data-srcset="srcSet"
         :sizes="`(max-width: ${ maxWidth }px) 100vw, ${ maxWidth }px`"
-        class="lazyload"
+        class="lazyload rounded-lg"
         :alt="alt"
     />
 </template>
