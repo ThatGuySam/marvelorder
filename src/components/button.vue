8d29387ec26cd2472332abddb3de842173fa1178
Sam Carlton <sam@sam.lc>
2022-06-07 22:14:21 -0500

    Add simple button component

---
+++ src/components/button.vue
@@ -0,0 +1,10 @@
+<template>
+    <button 
+        :class="[
+            'flex justify-center items-center hover:scale-10 bg-black/25 backdrop-blur rounded border border-solid border-white px-2 py-1', 
+            'transition-all duration-500 ease-in-out',
+        ]" 
+    >
+        <slot />
+    </button>
+</template>
