cf3a8f32968f87642386ab00b171a5099e724409
Sam Carlton <sam@sam.lc>
2022-06-13 16:34:07 -0500

    Fix listing content clipped at bottom

--- src/components/listing-content.vue
+++ src/components/listing-content.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col gap-8 pb-16">
+    <div class="flex flex-col gap-8">
         <div class="relative aspect-video w-full flex items-center px-12">
             <ListingLogo 
                 :src="listing"
