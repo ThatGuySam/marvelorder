f29beeaa51498405dd2050f7f0022b749fac82c9
Sam Carlton <sam@sam.lc>
2022-06-13 17:12:36 -0500

    Add Edit on GitHub button to Listing content

--- src/components/listing-content.vue
+++ src/components/listing-content.vue
@@ -21,6 +21,34 @@
             >Image provided by <a class="underline" href="https://fanart.tv/">Fanart.tv</a></div>
         </div>
 
+        <span class="relative z-0 inline-flex text-center md:flex-row flex-col shadow-sm md:divide-x md:divide-y-0 divide-y divide-gray-700 border border-gray-300 rounded-md bg-gray-900 md:py-3 md:px-0 px-4">
+            <a
+                v-for="(link, i) in links"
+                :key="i"
+                type="button"
+                :href="link.href"
+                :class="[
+                    'relative inline-flex justify-center items-center font-medium focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500',
+                    'text-white',
+                ]"
+            >
+                <div
+                    :class="[
+                        'inner-link hover:bg-indigo-400 active:bg-indigo-600 rounded-md px-4 md:py-2 md:mx-0 md:-my-3',
+                        'py-3 -mx-4',
+                        // First Link
+                        // i === 0 && 'rounded-l-md',
+                        // Not first Link
+                        i !== 0 ? 'md:-ml-px' : '',
+                        // Last Link
+                        // i === totalLinks - 1 ? 'rounded-r-md' : ''
+                    ]"
+                >
+                    {{ link.label }}
+                </div>
+            </a>
+        </span>
+
     </div>
 </template>
 
@@ -44,6 +72,14 @@ export default {
         hasFanartLogo
     },
     computed: {
+        links () {
+            return [
+                {
+                    label: 'Edit on GitHub',
+                    href: this.listing.githubEditUrl
+                },
+            ]
+        },
         backdropUrl () {
             return makeFunctionUrlFromTmdb( this.listing.backdrop_path )
         },
