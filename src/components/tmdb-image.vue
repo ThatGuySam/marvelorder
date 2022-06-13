a33b1f1ad40df94f96b42ebff4fce0c86be438db
Sam Carlton <sam@sam.lc>
2022-06-13 16:08:57 -0500

    Add tmdb-image component

---
+++ src/components/tmdb-image.vue
@@ -0,0 +1,69 @@
+<template>
+    <img 
+        v-once
+        :data-src="smallestImage"
+        :data-srcset="srcSet"
+        :sizes="`(max-width: ${ maxWidth }px) 100vw, ${ maxWidth }px`"
+        class="lazyload"
+        :alt="alt"
+    />
+</template>
+
+<script>
+
+import 'lazysizes'
+import 'lazysizes/plugins/attrchange/ls.attrchange'
+
+function makeSizedImageUrl ( imageUrl, size ) {
+    const sizeUrl = new URL( imageUrl, 'https://example.com' )
+
+    sizeUrl.searchParams.set( 'width', size )
+
+    return `${ sizeUrl.pathname }${ sizeUrl.search }`
+}
+
+export default {
+    props: {
+        src: {
+            type: String,
+            required: true
+        },
+        alt: {
+            type: String,
+            required: true
+        }, 
+        sizes: {
+            type: Array,
+            default: [ 75, 100, 275, 550, 850, 1440 ]
+        }
+    },
+    computed: {
+        maxWidth () {
+            return this.sizes[ this.sizes.length - 1 ]
+        },
+        imageUrl () {
+            // Get id from backdrop src url
+            const id = this.src
+                .split('/').pop()
+                .split('.').shift()
+
+            console.log('id', id)
+            
+            return `/.netlify/functions/tmdb-image/${ id }.webp?&transparent=0`
+        },
+		srcSet () {
+			const srcSet = []
+
+            for ( const size of this.sizes ) {
+                
+                srcSet.push( `${ makeSizedImageUrl( this.imageUrl, size ) } ${ size }w` )
+            }
+
+            return srcSet.join( ', ' )
+		},
+        smallestImage () {
+            return makeSizedImageUrl( this.imageUrl, this.sizes[ 0 ] ) 
+        },
+    }
+}
+</script>
