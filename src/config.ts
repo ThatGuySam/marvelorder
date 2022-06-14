ab6ef42ccaa7d6fe5d7eed00a3383be4da915860
Sam Carlton <sam@sam.lc>
2022-06-14 13:39:47 -0500

    Set OG image alt

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SITE = {
 export const OPEN_GRAPH = {
 	image: {
 		src: 'https://marvelorder.com/og-image.png',
-		alt: 'Marvel Order Text'
+		alt: 'MarvelOrder.com'
 	},
 	twitter: 'thatguysam',
 };
