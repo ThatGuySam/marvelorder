dc782f0e01b88d12b180901af0ef465de38cbc4d
Sam Carlton <sam@sam.lc>
2022-06-16 21:09:23 -0500

    Enable setting tags

--- scripts/add-amazon-links.ts
+++ scripts/add-amazon-links.ts
@@ -1,7 +1,22 @@
+import fs from 'fs-extra'
+
 // @ts-ignore
 import { upsertListingMarkdown } from '~/src/helpers/markdown-page.ts'
+import { 
+    getListingFiles,
+    getListingDetailsFromPaths,
+    getListingFromFile,
+    writeMarkdownFileNode
 // @ts-ignore
-import { getListingFiles } from '~/src/helpers/node/listing-files.ts'
+} from '~/src/helpers/node/listing-files.ts'
+// @ts-ignore
+import { ensureMappedListings } from '~/src/helpers/node/listing.ts'
+
+async function readMarkdownFileNode ( filePath: string ) {
+    // const markdownContent = await fs.readFile( filePath, 'utf8' )
+
+    return await getListingFromFile( filePath )
+}
 
 ;(async () => {
 
@@ -9,15 +24,25 @@ import { getListingFiles } from '~/src/helpers/node/listing-files.ts'
 
     const listingFiles = await getListingFiles()
 
-    console.log( 'listingFiles', listingFiles )
+    // console.log( 'listingFiles', listingFiles )
 
-    // await upsertListingMarkdown( {
-    //     listing,
-    //     readFile: async filePath =>  new TextDecoder( 'utf-8' ).decode( await Deno.readFile( filePath ) ),
-    //     writeMarkdownFile: writeMarkdownFileDeno,
-    //     exists: exists
-    // })
+    const rawListingsDetails = await getListingDetailsFromPaths( listingFiles )
 
+    // const mappedListings = ensureMappedListings( rawListings )
+
+    // console.log( 'allListings', mappedListings[0] )
+
+    rawListingsDetails[0].listing.tags = [ 'can-add-from-amazon-script' ]
+
+    // console.log( 'rawListingsDetails', rawListingsDetails[0] )
+
+    await upsertListingMarkdown( {
+        listing: rawListingsDetails[0].listing,
+        tmdb: rawListingsDetails[0].tmdb,
+        readMarkdownFile: readMarkdownFileNode,
+        writeMarkdownFile: writeMarkdownFileNode,
+        exists: fs.exists
+    })
 
     process.exit()
 })()
