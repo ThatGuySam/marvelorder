98aa60692db1b6731d10c779bd8d940d311c18f8
Sam Carlton <sam@sam.lc>
2022-06-17 17:36:31 -0500

    Try Pulling MCU Timeline Sheet during action

---
+++ scripts/pull-mcu-timeline-sheet.ts
@@ -0,0 +1,24 @@
+import fs from 'fs-extra'
+// import 'dotenv/config'
+
+// // @ts-ignore
+// import { upsertListingMarkdown } from '~/src/helpers/markdown-page.ts'
+// import { 
+//     getListingFiles,
+//     getListingDetailsFromPaths,
+//     getListingFromFile,
+//     writeMarkdownFileNode
+// // @ts-ignore
+// } from '~/src/helpers/node/listing-files.ts'
+// import { 
+// 	isUpcoming, 
+// 	FilteredListings
+// } from '~/src/helpers/listing-filters'
+
+
+;(async () => {
+
+    console.log('Script works!')
+    
+    process.exit()
+})()
