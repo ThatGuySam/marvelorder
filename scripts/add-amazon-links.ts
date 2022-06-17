5fb765c77aa22068bde60f6761860e9bca820d6e
Sam Carlton <sam@sam.lc>
2022-06-16 22:59:10 -0500

    Configure initial Amazon links functionality

--- scripts/add-amazon-links.ts
+++ scripts/add-amazon-links.ts
@@ -1,4 +1,5 @@
 import fs from 'fs-extra'
+import 'dotenv/config'
 
 // @ts-ignore
 import { upsertListingMarkdown } from '~/src/helpers/markdown-page.ts'
@@ -9,8 +10,17 @@ import {
     writeMarkdownFileNode
 // @ts-ignore
 } from '~/src/helpers/node/listing-files.ts'
-// @ts-ignore
-import { ensureMappedListings } from '~/src/helpers/node/listing.ts'
+
+const amazon = require('amazon-paapi')
+
+const commonParameters = {
+    AccessKey: process.env.AWS_ACCESS,
+    SecretKey: process.env.AWS_SECRET,
+    PartnerTag: process.env.AWS_TRACKING,
+    PartnerType: 'Associates', // Default value is Associates.
+    // Marketplace: 'www.amazon.com', // Default value is US. Note: Host and Region are predetermined based on the marketplace value. There is no need for you to add Host and Region as soon as you specify the correct Marketplace value. If your region is not US or .com, please make sure you add the correct Marketplace value.
+}
+
 
 async function readMarkdownFileNode ( filePath: string ) {
     // const markdownContent = await fs.readFile( filePath, 'utf8' )
@@ -20,29 +30,59 @@ async function readMarkdownFileNode ( filePath: string ) {
 
 ;(async () => {
 
-    // console.log('getListingFiles', getListingFiles)
-
     const listingFiles = await getListingFiles()
 
     // console.log( 'listingFiles', listingFiles )
 
     const rawListingsDetails = await getListingDetailsFromPaths( listingFiles )
 
-    // const mappedListings = ensureMappedListings( rawListings )
+    for ( const details of rawListingsDetails ) {
+        console.log( 'rentLinks', details.listing?.rentLinks )
+        // Skip listings that already have amazon links
+        if ( !!details.listing?.rentLinks?.amazon ) continue
 
-    // console.log( 'allListings', mappedListings[0] )
 
-    rawListingsDetails[0].listing.tags = [ 'can-add-from-amazon-script' ]
+        // console.log( 'listingDetails', listingDetails )
 
-    // console.log( 'rawListingsDetails', rawListingsDetails[0] )
+        console.log('details.listing.title', details.listing.title )
+          
+        const response = await amazon.SearchItems(commonParameters, {
+            Keywords: details.listing.title,
+            // Official Amazon List - https://webservices.amazon.com/paapi5/documentation/locale-reference/united-states.html#search-index
+            SearchIndex: 'AmazonVideo',
+            ItemCount: 5,
+            Resources: [
+                'Images.Primary.Medium',
+                'ItemInfo.Title',
+                'ItemInfo.Title',
+                'Offers.Listings.Price',
+            ],
+        })
+        .catch(err => {
+            console.log( 'Amazon Error', err )
+        })
 
-    await upsertListingMarkdown( {
-        listing: rawListingsDetails[0].listing,
-        tmdb: rawListingsDetails[0].tmdb,
-        readMarkdownFile: readMarkdownFileNode,
-        writeMarkdownFile: writeMarkdownFileNode,
-        exists: fs.exists
-    })
+
+        console.log( 'results', response.SearchResult.Items )
+        console.log( 'results', response.SearchResult.Items[0].ItemInfo.Title )
+
+        details.listing.rentLinks = {
+            amazon: response.SearchResult.Items[0].DetailPageURL,
+        }
+
+        await upsertListingMarkdown( {
+            listing: details.listing,
+            tmdb: details.tmdb,
+            readMarkdownFile: readMarkdownFileNode,
+            writeMarkdownFile: writeMarkdownFileNode,
+            exists: fs.exists,
+        })
+
+        break
+
+        // Wait for 2 seconds to avoid overloading Product Advertising API 1 request per second rate limit
+        await new Promise( resolve => setTimeout( resolve, 2000 ) )
+    }
 
     process.exit()
 })()
