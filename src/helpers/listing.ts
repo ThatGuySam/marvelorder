ffaac555c49d421fe30f836a05e3019274a356a8
Sam Carlton <sam@sam.lc>
2022-06-16 21:05:53 -0500

    Throw for missing id on endpoints

--- src/helpers/listing.ts
+++ src/helpers/listing.ts
@@ -3,6 +3,11 @@
 import { Listing } from './types.ts'
 
 export function makeListingEndpoint ( listing : Listing ) {
+    // Throw if id is not number
+    if ( typeof listing.id !== 'number' ) {
+        throw new Error( `Listing id must be a number and not empty. Got ${listing.id}` )
+    }
+
     return `/en/${ listing.slug }-${ listing.id }`
 }
 
