edb228488188ba8263898d3470ce9dfa0f28f694
Sam Carlton <sam@sam.lc>
2022-06-23 14:12:24 -0500

    Add updateMarkdownContent helper

--- src/helpers/node/readme.ts
+++ src/helpers/node/readme.ts
@@ -1,22 +1,50 @@
 import fs from 'fs-extra'
 
 
-export async function updateReadmeListContent ( newListMardown: string, markerString: string ) {
+function updateMarkdownContent ( options:any = {} ) {
+    const {
+        sourceMarkdown, 
+        newMarkdown,
+        markerString
+    } = options
+
     const startMarker = `<!-- start-${ markerString } -->`
     const endMarker = `<!-- end-${ markerString } -->`
 
+    // Throw if start marker is not found
+    
+
+    const startIndex = sourceMarkdown.indexOf( startMarker ) + startMarker.length
+    const endIndex = sourceMarkdown.indexOf( endMarker )
+
+    const newReadmeListContent = [
+        sourceMarkdown.slice( 0, startIndex ),
+        newMarkdown,
+        sourceMarkdown.slice( endIndex )
+    ].join('')
+
+    return newReadmeListContent
+}
+
+// function makeUpcomingListingsMarkdown ( upcomingListings: Listing[] ) {
+// }
+
+
+
+export async function updateReadmeListContent ( newListMardown: string, markerString: string ) {
 
     // Get README.md content
     const readmeContent = await fs.readFile( './README.md', 'utf8' )
 
-    console.log( 'readmeContent', readmeContent )
+    // console.log( 'readmeContent', readmeContent )
 
-    const startIndex = readmeContent.indexOf( startMarker ) + startMarker.length
-    const endIndex = readmeContent.indexOf( endMarker )
+    const newReadmeListContent = updateMarkdownContent({
+        sourceMarkdown: readmeContent,
+        newMarkdown: newListMardown,
+        markerString
+    })
 
-    const newReadmeListContent = readmeContent.slice( 0, startIndex ) + newListMardown + readmeContent.slice( endIndex )
-
-    console.log( 'newReadmeListContent', newReadmeListContent )
+    // console.log( 'newReadmeListContent', newReadmeListContent )
 
     await fs.writeFile( './README.md', newReadmeListContent )
 
