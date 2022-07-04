8d884a16af57fa9ebfacf257e5ebd3edcd48c9e1
Sam Carlton <sam@sam.lc>
2022-07-04 15:12:28 -0500

    Enable custom tag names on Circle button

--- src/components/circle-button.vue
+++ src/components/circle-button.vue
@@ -1,10 +1,22 @@
 <template>
-    <button 
+    <component
+        :is="tagName"
         :class="[
-            'flex justify-center items-center w-16 hover:w-20 aspect-square bg-black/25 backdrop-blur rounded-full border border-solid border-white', 
+            'flex justify-center items-center w-16 hover:w-20 aspect-square bg-black/25 backdrop-blur rounded-full border border-solid border-white',
             'transition-all duration-200 ease-in-out',
-        ]" 
+        ]"
     >
         <slot />
-    </button>
+    </component>
 </template>
+<script>
+
+export default {
+    props: {
+        tagName: {
+            type: String,
+            default: 'button'
+        }
+    }
+}
+</script>
