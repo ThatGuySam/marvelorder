596ca47fba40bd412607b70e31029c9f206b65b6
Sam Carlton <sam@sam.lc>
2022-07-04 14:38:06 -0500

    Add z helpers

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+            zIndex: {
+                'top-nav': '1000',
+                'overlay-nav': '900',
+            }
+        },
 	},
 	plugins: [ require('daisyui') ]
 }
