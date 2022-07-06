0067032b39c82b7fd6153ca4f3d9b4f689f0fded
Sam Carlton <sam@sam.lc>
2022-07-06 12:08:29 -0500

    Add Tailwind reference

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,7 @@ export default defineConfig({
         tailwind({
             // Example: Disable injecting a basic `base.css` import on every page.
             // Useful if you need to define and/or import your own custom `base.css`.
+            // https://github.com/withastro/astro/tree/main/packages/integrations/tailwind#configuration
             config: { applyBaseStyles: false },
         }),
         sitemap()
