a12cd63efdfe8629418f7c8e6cb014210f60caee
Sam Carlton <sam@sam.lc>
2022-07-24 23:39:26 -0500

    Add listing date helpers

--- src/helpers/listing.ts
+++ src/helpers/listing.ts
@@ -2,6 +2,67 @@
 // @ts-ignore
 import { Listing } from './types.ts'
 
+
+export function hasLogo ( listing:Listing ) {
+    return listing?.logo_on_black
+}
+
+export function getDateString ( listing:Listing ) {
+    return listing.release_date || listing.first_air_date
+}
+
+export function hasDate ( listing:Listing ) {
+    return !!getDateString( listing )
+}
+
+const seasonMonths = {
+    'winter': '11',
+    'spring': '04',
+    'summer': '08',
+    'fall': '09'
+}
+
+export function getSeasonReleaseDate ( listing:Listing ) {
+    if ( !hasDate( listing ) ) {
+        return null
+    }
+
+    const dateString = getDateString( listing )
+
+    for ( const seasonName of Object.keys( seasonMonths ) ) {
+        if ( dateString.toLowerCase().includes( seasonName ) ) {
+            return {
+                name: seasonName,
+                month: seasonMonths[ seasonName ]
+            }
+        }
+    }
+
+    return null
+}
+
+export function getIsoDate ( listing:Listing ) {
+    const dateString = getDateString( listing )
+    const season = getSeasonReleaseDate( listing )
+
+    if ( !!season ) {
+
+        // return DateTime.fromISO( `2023-07-01` )
+
+        console.log( 'dateString', dateString )
+
+        const [ seasonName, year ] = dateString.split(' ')
+
+        const isoDate = `${ year }-${ season.month }-01`
+
+        console.log( 'isoDate', isoDate )
+
+        return isoDate
+    }
+
+    return dateString
+}
+
 export function makeListingEndpoint ( listing : Listing ) {
     // Throw if id is not number
     if ( typeof listing.id !== 'number' ) {
