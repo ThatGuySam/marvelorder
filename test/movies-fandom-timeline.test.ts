ac187d3203a7408609798c298ec803949544575b
Sam Carlton <sam@sam.lc>
2022-08-31 21:03:14 -0500

    Test that Altered 2014 Timeline exists

--- test/movies-fandom-timeline.test.ts
+++ test/movies-fandom-timeline.test.ts
@@ -11,15 +11,20 @@ beforeAll(async () => {
     timeline = await getTimeline()
 })
 
-test('Can get timeline entries', async () => {
+test( 'Can get timeline entries', async () => {
 
     expect( timeline.entries ).toBeDefined()
     expect( timeline.entries.length ).toBeGreaterThan( 0 )
 })
 
-test('Can get timeline entry from entries', () => {
+test( 'Can get timeline entry from entries', () => {
     const firstEntry = timeline.entries[ 0 ]
 
     expect( firstEntry ).toBeDefined()
-
 })
+
+test( 'Can find Altered 2014 Timeline', () => {
+    const altered2014Timeline = timeline.entries.find( entry => entry.timeline === 'Altered 2014 Timeline' )
+
+    expect( altered2014Timeline ).toBeDefined()
+} )
