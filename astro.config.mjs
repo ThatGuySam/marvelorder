605a1dfc4eb2578f6e7c8f2450085c8c3e98be0d
ThatGuySam <sam@sam.lc>
2023-07-02 17:16:03 -0500

    Switch astro to server mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
 
+import { netlifyFunctions } from '@astrojs/netlify'
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'server',
+    adapter: netlifyFunctions( {
+        dist: new URL( './dist/', import.meta.url ),
+    } ),
     site: 'https://marvelorder.com',
     integrations: [// Enable Preact to support Preact JSX components.
         preact(), // Enable React for the Algolia search component.
