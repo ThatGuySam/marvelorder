9b269b31e7cdc155f5514c09d227e3c3ec3787bf
ThatGuySam <sam@sam.lc>
2023-07-02 18:17:49 -0500

    Disable threads to fix canvas error

--- vitest.config.js
+++ vitest.config.js
@@ -5,6 +5,9 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
     plugins: [ tsconfigPaths() ], 
     test: {
+        // Fixes jsdom/canvas error
+        // https://github.com/vitest-dev/vitest/issues/740#issuecomment-1042648373
+        threads: false
         // ...
     },
 })
